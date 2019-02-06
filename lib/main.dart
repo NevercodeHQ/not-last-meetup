7eb4db3b3a6a11c835993309fa91f1dceffc254f
andrijasinski <andri.jasinski@gmail.com>
2019-02-06 12:10:59 +0200

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter LDN Demo',
+      title: 'Not Last Meetup',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
